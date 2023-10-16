b9ce061b8128f07510801c695967fd3135ac8e7f
Moises088 <costamoises300@gmail.com>
2023-10-16 00:15:17 -0300

    fix: fixed dark theme in home types

--- src/app/shared/services/theme.service.ts
+++ src/app/shared/services/theme.service.ts
@@ -11,14 +11,16 @@ export class ThemeService {
     background: "#0D1117",
     text: "#FFF",
     textSecondary: "#c2c2c2",
-    textMuted: "#ebebeb"
+    textMuted: "#ebebeb",
+    btnGrayLinear: "linear-gradient(104deg, rgb(27, 29, 30), rgb(34, 36, 38))"
   }
   
   private readonly light = {
     background: "#FFF",
     text: "#000",
     textSecondary: "#37474F",
-    textMuted: "#0D2317"
+    textMuted: "#0D2317",
+    btnGrayLinear: "linear-gradient(104deg, #fafafa, #eee)"
   }
 
   constructor() { }
@@ -33,11 +35,13 @@ export class ThemeService {
       document.documentElement.style.setProperty('--text-primary', this.dark.text);
       document.documentElement.style.setProperty('--text-secondary', this.dark.textSecondary);
       document.documentElement.style.setProperty('--text-muted', this.dark.textMuted);
+      document.documentElement.style.setProperty('--btn-gray-linear', this.dark.btnGrayLinear);
     } else {
       document.documentElement.style.setProperty('--background-primary', this.light.background);
       document.documentElement.style.setProperty('--text-primary', this.light.text);
       document.documentElement.style.setProperty('--text-secondary', this.light.textSecondary);
       document.documentElement.style.setProperty('--text-muted', this.light.textMuted);
+      document.documentElement.style.setProperty('--btn-gray-linear', this.light.btnGrayLinear);
     }
   }
 
